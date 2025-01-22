6d3c1abf53836b09cdf6bcf33f862b4ded9cbec7
Kimsungbin1 <ksb19558@naver.com>
2025-01-23 04:12:52 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+# 데이터베이스 연결 대기
+echo "Waiting for database..."
+max_retries=30
+counter=0
+
+while [ $counter -lt $max_retries ]
+do
+    pg_isready -h $(echo $DATABASE_URL | cut -d'@' -f2 | cut -d'/' -f1 | cut -d':' -f1) \
+               -p $(echo $DATABASE_URL | cut -d'@' -f2 | cut -d'/' -f1 | cut -d':' -f2)
+    if [ $? -eq 0 ]; then
+        echo "Database is ready!"
+        break
+    fi
+    echo "Waiting for database... Attempt $((counter+1))/$max_retries"
+    counter=$((counter+1))
+    sleep 2
+done
+
+if [ $counter -eq $max_retries ]; then
+    echo "Could not connect to database after $max_retries attempts"
+    exit 1
+fi
+
 # 데이터베이스 마이그레이션 실행
 echo "Running database migrations..."
 alembic upgrade head
