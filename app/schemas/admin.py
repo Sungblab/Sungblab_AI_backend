49a41ccf0ecb135c85bfb6db55490bf1f25c366b
Kimsungbin1 <ksb19558@naver.com>
2025-03-05 17:21:59 +0900

    Update admin.py

--- app/schemas/admin.py
+++ app/schemas/admin.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 from pydantic import BaseModel
 from datetime import datetime
+from app.models.subscription import SubscriptionPlan
 
 class UserResponse(BaseModel):
     id: str
